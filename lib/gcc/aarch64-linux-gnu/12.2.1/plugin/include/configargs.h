2e707fac1d0bb14592fbda84544ccfb1886d0597
WisnuArdhi28 <wisnuardi128@gmail.com>
2023-07-25 00:53:36 +0700

    import LINARO GCC 12.2.1

---
+++ lib/gcc/aarch64-linux-gnu/12.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure CFLAGS='-O3 -fno-stack-protector' CXXFLAGS='-O3 -fno-stack-protector' --prefix=/home/wisnu/build-tools-gcc/aarch64-linux-gnu --target=aarch64-linux-gnu --with-glibc-version=2.37 --with-pkgversion='Arm GNU Toolchain 12.2.rel1 (build-tools-gcc r320-g6cdd090e)' --disable-werror --with-bugurl=https://github.com/krasCGQ/build-tools-gcc/issues --enable-fix-cortex-a53-835769 --with-zstd=/home/wisnu/build-tools-gcc/aarch64-linux-gnu --disable-bootstrap --disable-libmpx --disable-libssp --disable-multilib --disable-nls --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
